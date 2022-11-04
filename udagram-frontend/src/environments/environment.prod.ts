5c3033694dba9db4de31588b6eaa50098be014ae
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-05 00:09:28 +0300

    remove production api urls

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'localhost:3000/api/v0',
 };
 
 /*
